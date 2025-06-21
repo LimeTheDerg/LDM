c4e81fd42e4d152a8ad7d5529ac95ac2bb1686b4
LimeTheDerg <emilsahel@gmail.com>
2025-06-20 22:50:16 -0400

    Added list daemon functionality and went to war with cmake...

--- src/interface.cpp
+++ src/interface.cpp
@@ -11,26 +11,27 @@ int main(int argc, char *argv[]) {
     // Checks to see if an adequate number of arguments were given
     if (argc > 3 || argc < 2) {
         cerr << "----------ERROR----------" << endl <<
-                "Improper number of arguments, use command 'ldmi help' to learn proper usage." << endl;
+                "Improper number of arguments, use command 'ldmi help' for proper usage." << endl;
         return 1;
     }
 
     // Initialize the terms of the operation
     const string operation = argv[1];
-    const string target = argv[2];
+    const string target = argc > 2 ? argv[2] : "";
+    json cache = read_cache(); // Read from cache
 
-    if (operation == "help") {
+
+    if (operation == "help" && argc == 2) {
         // TODO: Create help.txt file and read from it here.
     }
 
-    if (operation == "start") {
-
-        json cache = read_cache(); // Read from cache
-
+    if (operation == "list" && argc == 2) {
+        cout << "Available daemons:" << endl;
         for (int i = 0; i < cache.size(); i++) {
-            cout << cache["daemons"][i]["conf_msg"] << endl;
+            const string str = cache["daemons"][i]["name"];
+            cout << str << endl;
         }
-    }
+        return 0;
 
     return 0;
 }

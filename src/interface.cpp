b8b0aea3d0bbaefd2120b9e83e5fb2f7b6c59db6
LimeTheDerg <emilsahel@gmail.com>
2025-06-23 12:53:54 -0400

    Fixed everything after losing a pile of work...

--- src/interface.cpp
+++ src/interface.cpp
@@ -3,22 +3,31 @@
 #include "lib/json/json.hpp"
 #include "lib/util/helper_functions.hpp"
 
-using namespace std;
+using std::cout;
+using std::cerr;
+using std::string;
+
 using json = nlohmann::json;
 
 int main(int argc, char *argv[]) {
 
     // Checks to see if an adequate number of arguments were given
     if (argc > 3 || argc < 2) {
-        cerr << "----------ERROR----------" << endl <<
-                "Improper number of arguments, use command 'ldmi help' for proper usage." << endl;
+        cerr << "----------ERROR----------" << "\n" <<
+                "Improper number of arguments, use command 'ldmi help' for proper usage." << "\n";
         return 1;
     }
 
     // Initialize the terms of the operation
     const string operation = argv[1];
     const string target = argc > 2 ? argv[2] : "";
-    json cache = read_cache(); // Read from cache
+    json cache;
+
+    try {
+        cache = read_cache(); // Read from cache
+    } catch (const std::exception &e) {
+        cout << "Error reading from cache file: " << e.what() << "\n";
+    }
 
 
     if (operation == "help" && argc == 2) {
@@ -26,10 +35,10 @@ int main(int argc, char *argv[]) {
     }
 
     if (operation == "list" && argc == 2) {
-        cout << "Available daemons:" << endl;
+        cout << "Available daemons:" << "\n";
         for (int i = 0; i < cache.size(); i++) {
             const string str = cache["daemons"][i]["name"];
-            cout << str << endl;
+            cout << str << "\n";
         }
         return 0;
     }
--- src/lib/util/helper_functions.hpp
+++ src/lib/util/helper_functions.hpp
@@ -1,3 +1,26 @@
 #ifndef HELPER_FUNCTIONS_H
 #define HELPER_FUNCTIONS_H
 #endif
+
+#include "../json/json.hpp"
+
+inline std::string find_process_path() {
+    char path[PATH_MAX];
+    ssize_t count = readlink("/proc/self/exe", path, sizeof(path) - 1);
+    if (count != -1) {
+        path[count] = '\0';
+        const std::string path_str = path;
+        return path_str.substr(0, path_str.find_last_of('/'));
+    }
+    std::cerr << "Something has gone catastrophically wrong.";
+}
+
+inline nlohmann::json read_cache() {
+    std::ifstream cache_file;
+    cache_file.open(find_process_path() + "/daemon_cache.json");
+    if (!cache_file.is_open()) {
+        std::cerr << "Failed to open cache file" << "\n";
+    }
+    nlohmann::json cache_json = nlohmann::json::parse(cache_file);
+    return cache_json;
+}

06b3530060592a9e83307a1abe22ec2a79758b91
LimeTheDerg <emilsahel@gmail.com>
2025-07-02 18:16:43 -0400

    forgot i was in bin not root ;-;

--- uninstall.sh
+++ uninstall.sh
@@ -3,9 +3,13 @@ set -e
 
 if [ -d LDM ]; then
   BIN_PATH=$(realpath LDM)
-  rm -rf LDM
+  rm -rf LDM && echo "Deleted LDM directory at $BIN_PATH"
 else
   echo "LDM does not exist — are you in the wrong directory?"
 fi
 
+cp "$HOME/.bashrc" "$HOME/.bashrc.backup"
+
+grep -vF "$BIN_PATH" "$HOME/.bashrc" > "$HOME/.bashrc.tmp" && mv "$HOME/.bashrc.tmp" "$HOME/.bashrc" || echo "An error has occurred when trying to remove LDM from PATH"
+echo "Removal complete, ~/.bashrc has been modified, and if something has gone wrong, there is a backup at ~/.bashrc.backup"
 
